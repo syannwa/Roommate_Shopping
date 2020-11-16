23046cabff1fc575974bb9fbed341fcd186ea533
Sydney Waters <saw465712@uga.edu>
2020-11-16 07:21:42 -0500

    Completed RegisterActivity

--- app/src/main/java/edu/uga/cs/roommateshopping/MainActivity.java
+++ app/src/main/java/edu/uga/cs/roommateshopping/MainActivity.java
@@ -2,13 +2,41 @@ package edu.uga.cs.roommateshopping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button registerButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerButton = findViewById(R.id.splashRegisterButton);
+        loginButton = findViewById(R.id.splashLoginButton);
+
+        registerButton.setOnClickListener(new RegisterButtonClickListener());
+        loginButton.setOnClickListener(new LoginButtonClickListener());
+    }
+
+    public class RegisterButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(v.getContext(), RegisterActivity.class);
+            v.getContext().startActivity(intent);
+        }
+    }
+
+    public class LoginButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+//            Intent intent = new Intent(v.getContext(), new LoginActivity.class);
+//            v.getContext().startActivity(intent);
+        }
     }
 }
--- app/src/main/java/edu/uga/cs/roommateshopping/RegisterActivity.java
+++ app/src/main/java/edu/uga/cs/roommateshopping/RegisterActivity.java
@@ -2,6 +2,9 @@ package edu.uga.cs.roommateshopping;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,19 +15,32 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthMultiFactorException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.MultiFactorResolver;
 
 public class RegisterActivity extends AppCompatActivity {
 
     public static final String TAG = "Sign in Activity";
 
     private FirebaseAuth mAuth;
+    private Button register;
+
+    private EditText emailText;
+    private EditText passwordText;
+    //private ActivityEmailpasswordBinding mBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
         mAuth = FirebaseAuth.getInstance();
+        register = findViewById(R.id.registerButton);
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+
+        register.setOnClickListener(new ButtonClickListener());
     }
 
     @Override
@@ -32,6 +48,16 @@ public class RegisterActivity extends AppCompatActivity {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         //updateUI(currentUser);
+    }
+
+    private class ButtonClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            String email = emailText.getText().toString();
+            String password = passwordText.getText().toString();
+            createAccount(email, password);
+        }
     }
 
     private void createAccount(String email, String password) {
@@ -59,4 +85,35 @@ public class RegisterActivity extends AppCompatActivity {
                 });
         // [END create_user_with_email]
     }
+
+//    private void sendEmailVerification() {
+//        // Disable button
+//        //mBinding.verifyEmailButton.setEnabled(false);
+//
+//        // Send verification email
+//        // [START send_email_verification]
+//        final FirebaseUser user = mAuth.getCurrentUser();
+//        user.sendEmailVerification()
+//                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        // [START_EXCLUDE]
+//                        // Re-enable button
+//                        //mBinding.verifyEmailButton.setEnabled(true);
+//
+//                        if (task.isSuccessful()) {
+//                            Toast.makeText(RegisterActivity.this,
+//                                    "Verification email sent to " + user.getEmail(),
+//                                    Toast.LENGTH_SHORT).show();
+//                        } else {
+//                            Log.e(TAG, "sendEmailVerification", task.getException());
+//                            Toast.makeText(RegisterActivity.this,
+//                                    "Failed to send verification email.",
+//                                    Toast.LENGTH_SHORT).show();
+//                        }
+//                        // [END_EXCLUDE]
+//                    }
+//                });
+//        // [END send_email_verification]
+//    }
 }
